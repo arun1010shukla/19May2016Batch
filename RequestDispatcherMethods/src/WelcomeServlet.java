8c932204eeb1bfaf4fe9b1b72d5dffee53a4748a
Degaonkar <sanjay.degaonkar@capgemini.com>
2016-07-14 16:23:57 +0530

    Project created by participant

---
+++ RequestDispatcherMethods/src/WelcomeServlet.java
@@ -0,0 +1,58 @@
+
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class WelcomeServlet
+ */
+@WebServlet("/WelcomeServlet")
+public class WelcomeServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public WelcomeServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		response.getWriter().append("Served at: ").append(request.getContextPath());
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		response.setContentType("text/html");  
+	    PrintWriter out = response.getWriter();  
+	          
+	    String n=request.getParameter("userName");  
+	    String p=request.getParameter("userPass");  
+	          
+	    if(p.equals("123")){  
+	        RequestDispatcher rd=request.getRequestDispatcher("Login");  
+	        rd.forward(request, response);  
+	    }  
+	    else{  
+	        out.print("Sorry UserName or Password Error!");  
+	        RequestDispatcher rd=request.getRequestDispatcher("/index.html");  
+	        rd.forward(request, response);  
+	                      
+	        }  
+	}
+
+}
